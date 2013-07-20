8cc4bf5a2a80e391c65c8c5112f329db975649ab
Shimamoo <hiroki.shimamoto@gmail.com>
2013-07-21 03:33:12 +0900

    update option

--- MCP4922.h
+++ MCP4922.h
@@ -5,15 +5,14 @@
 class MCP4922
 {
 public:
-    MCP4922(unsigned int LDAC, unsigned int SS);
-    void begin(unsigned int selectDAC, unsigned int BUF, unsigned int GA, unsigned int SHDN);
-    void set(unsigned int val);
+	MCP4922(unsigned int LDAC, unsigned int SS);
+	void begin();
+	void set(unsigned int selectDAC, unsigned int BUF, unsigned int GA, unsigned int SHDN, unsigned int val);
+	void update();
 private:
-    int _SS;
-    int _LDAC;
-    unsigned int _settingVal;
+	int _SS;
+	int _LDAC;
+	unsigned int _settingVal;
 };
 
 #endif
-
-
