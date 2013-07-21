3ff3545298899becf17713afd7ece880dc3f88c1
Shimamoo <hiroki.shimamoto@gmail.com>
2013-07-21 14:56:13 +0900

    update example

--- example/example.ino
+++ example/example.ino
@@ -1,3 +1,7 @@
+//
+//MCP4921 and MCP4922
+//
+
 #include "MCP492X.h"
 #include <SPI.h>
 
@@ -5,21 +9,18 @@
 //SCK -> SCK(D13/D52)
 //SDI -> MOSI(D11/D51)
 
-
 MCP492X DAC1(9, 10);
 MCP492X DAC2(9, 10);
 //(LDAC pin, CS pin)
 
 void setup() {
-  
   //begin
   DAC1.begin();
   DAC2.begin();
-  
 }
 
 void loop() {
-  
+  //set
   DAC1.set(0, 1, 1, 1, 1023);
   DAC2.set(1, 1, 1, 1, 4095);
   // (!A/B, BUF, !GA, !SHDN, OutputVal)
@@ -29,7 +30,8 @@ void loop() {
   // !SHDN : Output Power Down Control
   // OutputVal is 0 to 4095
   
+  //Update Output
   DAC1.update();
   DAC2.update();
-  //Update Output
+  
 }
